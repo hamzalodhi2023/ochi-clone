9a27745646e50a6883af25a68cf3c7f2748154fb
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-03-16 13:03:51 +0500

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="w-fll h-screen bg-zinc-900"></div>;
+  return (
+    <div className="w-fll h-screen bg-zinc-900 text-white">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
