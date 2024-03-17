1b673f95e53a26e38e14bebd6195c12b823c7ccd
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-03-17 15:08:38 +0500

    about compnent created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
 import Marquee from "./components/Marquee";
+import About from "./components/About";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <LandingPage />
       <Marquee />
+      <About />
     </div>
   );
 }
