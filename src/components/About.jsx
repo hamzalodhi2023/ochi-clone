3631e9d4df7e38ac65f701d0122ee591377de650
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-03-17 15:22:54 +0500

    working on About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="w-full p-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-black">
+      <h1 className="text-4xl font-semibold leading-[3.5vw]">
+        Ochi is a strategic partner for fast-growing tech businesses that need
+        to raise funds, sell products, explain complex ideas, and hire great
+        people.
+      </h1>
+    </div>
+  );
 }
 
 export default About;
