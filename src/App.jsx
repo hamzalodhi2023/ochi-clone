afa5e3b82877a88081666e604226b71c0c398ab9
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-03-17 13:07:08 +0500

    marquee compnent created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
+import Marquee from "./components/Marquee";
 
 function App() {
   return (
     <div className="w-fll h-screen  text-white">
       <Navbar />
       <LandingPage />
+      <Marquee />
     </div>
   );
 }
---
+++ src/components/Marquee.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Marquee() {
+  return <div className="w-full h-[50vh] bg-[#004D43]"></div>;
+}
+
+export default Marquee;
