f2f5ed369aa03d250b61d54bf39f28357be4235b
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-03-18 13:47:13 +0500

    about section has been comoleted

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,10 +8,20 @@ function About() {
         to raise funds, sell products, explain complex ideas, and hire great
         people.
       </h1>
-      <div className="w-full border-t-[1px] mt-20 border-[#859743]">
+      <div className="flex gap-5 w-full border-t-[1px] pt-20 mt-20 border-[#859743]">
         <div className="w-1/2 ">
-          <h1 className="text-6xl ">Our approach:</h1>
-          <button>READ MORE</button>
+          <h1 className="text-7xl ">Our approach:</h1>
+          <button className=" flex gap-10 items-center px-10  py-6 bg-zinc-900 rounded-full text-white mt-10">
+            READ MORE
+            <div className="w-2 h-2 bg-zinc-100 rounded-full"></div>
+          </button>
+        </div>
+        <div className="w-1/2 h-[70vh] rounded-3xl bg-red-700 overflow-hidden">
+          <img
+            src="src/assets/Homepage-Photo-663x469.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
         </div>
       </div>
     </div>
