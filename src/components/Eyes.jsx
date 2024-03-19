1f6f2d15ce6526b65e65185ab0f3d7134ceb15e3
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-03-19 14:40:14 +0500

    working on eyes component

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function Eyes() {
-  return <div></div>;
+  return (
+    <div className="w-full h-screen overflow-hidden">
+      <div className="relative flex items-center justify-center w-full h-full bg-red-500 bg-cover bg-center bg-[url('src/assets/Top-Viewbbcbv-1-1440x921.jpg')]">
+        <div className="absolute flex gap-10 ">
+          <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
+            <div className="relative w-2/3 h-2/3 rounded-full bg-zinc-900">
+              <div className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] line w-full h-10 bg-red-500">
+                <div className="w-10 h-10 rounded-full bg-zinc-100"></div>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
+            <div className="relative w-2/3 h-2/3 rounded-full bg-zinc-900">
+              <div className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] line w-full h-10 bg-red-500">
+                <div className="w-10 h-10 rounded-full bg-zinc-100"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Eyes;
