7b142acb3b51b3474b69d2fdeefd631f6a89f103
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-03-21 13:38:11 +0500

    poblem

--- src/components/Card_Right.jsx
+++ src/components/Card_Right.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-function Card_Left({ card_1_Data }) {
+function Card_Right({ card_2_Data }) {
   return (
     <div className="relative card w-[45%] h-[70vh] rounded-2xl  ">
-      <h1 className="absolute z-[99] top-1/2 -translate-y-1/2 text-8xl left-full -translate-x-1/2 font-semibold text-[#CDEA68]">
-        {card_1_Data.text.split("").map((item, index) => (
+      <h1 className="absolute z-[99] top-1/2 -translate-y-1/2 text-8xl right-full translate-x-1/2 font-semibold text-[#CDEA68]">
+        {card_2_Data.text.split("").map((item, index) => (
           <span>{item}</span>
         ))}
       </h1>
       <div className="w-full h-full overflow-hidden rounded-2xl hover:scale-[0.9] transition-all">
         <img
           className="w-full h-full object-cover"
-          src={card_1_Data.imageSource}
+          src={card_2_Data.imageSource}
           alt=""
         />
       </div>
@@ -19,4 +19,4 @@ function Card_Left({ card_1_Data }) {
   );
 }
 
-export default Card_Left;
+export default Card_Right;
--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card_Left from "./Card_Left";
+import Card_Right from "./Card_Right";
 
 function Featured() {
   const card_1_Data = [
@@ -9,14 +10,31 @@ function Featured() {
       text: "FYDE",
     },
   ];
+  const card_2_Data = [
+    {
+      imageSource:
+        "https://ochi.design/wp-content/uploads/2022/09/Vise_front2-663x551.jpg",
+      text: "VISE",
+    },
+  ];
+
+  const cardsArr = [card_1_Data];
+
   return (
     <div className="w-full py-20 ">
       <div className="w-full px-20 border-b-[1px] border-zinc-700 pb-20">
         <h1 className="text-6xl tracking-tight ">Featured projects</h1>
       </div>
-      <div className="cards flex gap-10 px-20 py-20 ">
-        {card_1_Data.map((item, index) => (
-          <Card_Left card_1_Data={item} />
+      <div className="w-full cards flex justify-evenly flex-wrap py-20 ">
+        {cardsArr.map((card, i) => (
+          <>
+            {card.map((item, index) => (
+              <Card_Left key={index} card_1_Data={item} />
+            ))}
+            {card.map((item, index) => (
+              <Card_Right key={index} card_2_Data={item} />
+            ))}
+          </>
         ))}
       </div>
     </div>
